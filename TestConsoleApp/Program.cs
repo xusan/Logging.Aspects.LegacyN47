57e6feea9d00cb81ee9fbf0c7b96db7402520b11
xusan <xusanrahimov@gmail.com>
2026-05-14 12:37:39 +0500

    added tests

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cls = new TestingClass();
+            cls.Method1();
+            cls.Method2();
+            cls.Method3();
+        }
+    }
+}
---
+++ TestConsoleApp/TestingClass.cs
@@ -0,0 +1,26 @@
+﻿using Logging.Aspects;
+using System;
+
+namespace TestConsoleApp
+{
+    [LogMethods]
+    public class TestingClass
+    {
+        int d;
+        public void Method1()
+        {
+            d++;
+        }
+
+        public void Method2()
+        {
+            d++;
+        }
+
+        public void Method3()
+        {
+            d++;
+            throw new Exception("Test error");
+        }
+    }
+}
